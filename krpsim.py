578e8d6642c9ce8ece2559e14505ec58fe3362f1
arthur laine <arthur.laine99@gmail.com>
2020-10-22 12:08:11 +0200

    ressources

---
+++ krpsim.py
@@ -0,0 +1,32 @@
+import sys
+
+class   Process():
+    def __init__(self):
+        self.list_process = [] #liste de liste avec dans chaque sous liste un name et needs_qty et result_qty
+        # needs_qty liste de dico, pareil pour result_qty
+
+class   Stock():
+    def __init__(self):
+        self.list_stocks = [] #liste de liste avec nom du stock et qty + mettre a jour a chque action de process
+
+    def init_stocks(self, ressource):
+        self.set_list_stocks(self, ressource)
+
+#-------------------------------------------------------------------------
+# Getteurs et setteurs
+
+    def set_list_stocks(self, ressource):
+        self.list_stocks = parsing.init_stocks(ressource)
+
+    def get_list_stocks(self):
+        return self.list_stocks
+
+#
+#-------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Wrong number of arguments.")
+        sys.exit()
+    stocks = Stock()
+    stocks.init_stocks(sys.argv[1])
---
+++ parsing.py
@@ -0,0 +1,5 @@
+import sys
+
+def init_stocks(ressource):
+    """Parsing du fichier source et initialisation des stocks"""
+    return None
