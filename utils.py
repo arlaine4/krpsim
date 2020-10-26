7a214ed74f1053e5bf4fe8d8909089350ee99c57
arthur laine <arthur.laine99@gmail.com>
2020-10-26 16:31:49 +0100

    custom affichage pre algo

--- utils.py
+++ utils.py
@@ -4,21 +4,52 @@ import time
 def print_pre_infos(stocks, process, optimize):
     len_stocks = p.get_stock_process(stocks, process)
     print("\n- \033[4mFile informations\033[0m :\n")
-    print("#-------------------------------------------#")
+    print("\n\033[38;5;204m#\033[0m",end='')
+    for k in range(10):
+        for i in range(196, 201):
+            print("\033[38;5;{}m-\033[0m".format(i), end='')
+            for j in range(201, 196):
+                print("\033[38;5;{}m-\033[0m".format(j), end='')
+    print("\033[38;5;204m#\033[0m",end='')
+    print("\n")
     print("{} \033[1mprocesses\033[0m / {} \033[1mstocks\033[0m / {} to \033[1moptimize\033[0m.".format(len(process), len_stocks, len(optimize)))
-    print("#-------------------------------------------#\n")
+    print("\n\033[38;5;204m#\033[0m",end='')
+    for k in range(10):
+        for i in range(196, 201):
+            print("\033[38;5;{}m-\033[0m".format(i), end='')
+            for j in range(201, 196):
+                print("\033[38;5;{}m-\033[0m".format(j), end='')
+    print("\033[38;5;204m#\033[0m",end='')
+    print("\n")
     print("- \033[4mStocks\033[0m :\n")
-    #for i in range(16, 20):
-        #print("\033[48;5;{}m-\033[0m".format(i), end='')
     for tmp in stocks:
-        #print("\033[48;5;21m>\033[0m",end='')
         print("\033[0;49m{}\033[0m : \033[1;49;32m{}\033[0m".format(tmp, stocks[tmp]))
-    print("\n#-------------------------------------------#\n")
+    print("\n\033[38;5;204m#\033[0m",end='')
+    for k in range(10):
+        for i in range(196, 201):
+            print("\033[38;5;{}m-\033[0m".format(i), end='')
+            for j in range(201, 196):
+                print("\033[38;5;{}m-\033[0m".format(j), end='')
+    print("\033[38;5;204m#\033[0m",end='')
+    print("\n")
     print("- \033[4mProcesses\033[0m :\n")
     for tmp in process:
         print("\033[1m{}\033[0m : {} : {} : {}".format(tmp[0], tmp[1], tmp[2], tmp[3]))
-    print("\n#-------------------------------------------#\n")
+    print("\n\033[38;5;204m#\033[0m",end='')
+    for k in range(10):
+        for i in range(196, 201):
+            print("\033[38;5;{}m-\033[0m".format(i), end='')
+            for j in range(201, 196):
+                print("\033[38;5;{}m-\033[0m".format(j), end='')
+    print("\033[38;5;204m#\033[0m",end='')
+    print("\n")
     print("- \033[4mTo optimize\033[0m :\n")
     for tmp in optimize:
         print("\033[1m{}\033[0m".format(tmp))
-    print("\n#-------------------------------------------#\n")
+    print("\n\033[38;5;204m#\033[0m",end='')
+    for k in range(10):
+        for i in range(196, 201):
+            print("\033[38;5;{}m-\033[0m".format(i), end='')
+            for j in range(201, 196):
+                print("\033[38;5;{}m-\033[0m".format(j), end='')
+    print("\033[38;5;204m#\033[0m")
